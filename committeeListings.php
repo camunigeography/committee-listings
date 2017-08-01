camunigeography/committee-listings

commit 4f7f2132ae8d189d57bac5ea6da8c394e3e12d71

Indicate when committee is staff-only; implementation pending

committeeListings.php
@@ -113,6 +113,7 @@ class committeeListings extends frontControllerApplication
 			  `moniker` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT 'URL moniker',
 			  `prefixFilename` VARCHAR(255) NOT NULL COLLATE utf8_unicode_ci COMMENT 'Document prefix'
 			  `typeId` INT(11) NOT NULL COMMENT 'Type',
+			  `staffOnly` INT(1) NULL DEFAULT NULL COMMENT 'Staff only?',
 			  `managers` VARCHAR(255) NULL DEFAULT NULL COMMENT 'Managers (usernames, one per line)',
 			  `ordering` ENUM('1','2','3','4','5','6','7','8','9') CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '5' COMMENT 'Ordering (1 = first)',
 			  `spaceAfter` INT(1) NULL COMMENT 'Add space after?',
@@ -270,10 +271,13 @@ class committeeListings extends frontControllerApplication
 				$listingHtml .= "\n<h2>" . htmlspecialchars ($type) . '</h2>';
 			}
 			
+			# Define a marker for indicating that an area is staff only
+			$restrictionMarkerHtml = " <img src=\"/images/icons/shield.png\" border=\"0\" class=\"restricted\" title=\"Staff-only area\" />";
+			
 			# Create the list for this type
 			$list = array ();
 			foreach ($committees as $moniker => $committee) {
-				$list[] = "<a href=\"{$committee['path']}/\"" . ($committee['isExternal'] ? ' target="_blank"' : '') . ($committee['spaceAfter'] ? ' class="spaced"' : '') . '>' . htmlspecialchars ($committee['name']) . '</a>';
+				$list[] = "<a href=\"{$committee['path']}/\"" . ($committee['isExternal'] ? ' target="_blank"' : '') . ($committee['spaceAfter'] ? ' class="spaced"' : '') . '>' . htmlspecialchars ($committee['name']) . '</a>' . ($committee['staffOnly'] ? $restrictionMarkerHtml : '');
 			}
 			$listingHtml .= application::htmlUl ($list);
 		}

styles.css
@@ -12,3 +12,5 @@
 #committeelistings .actions a:hover, #committeelistings table.meetings tr td.edit a:hover {background-color: #ddd; color: black; text-decoration: none;}
 
 #committeelistings ul.tabs li a.empty {opacity: 0.6;}
+
+#committeelistings img.restricted {opacity: 0.6;}

commit 3a9cec1b291dcfae8b3455ab6051c55e791f2d19

Amend index listing spacing following regression in 4f7f2132ae8d189d57bac5ea6da8c394e3e12d71

committeeListings.php
@@ -335,9 +335,9 @@ class committeeListings extends frontControllerApplication
 			# Create the list for this type
 			$list = array ();
 			foreach ($committees as $moniker => $committee) {
-				$list[] = "<a href=\"{$committee['path']}/\"" . ($committee['isExternal'] ? ' target="_blank"' : '') . ($committee['spaceAfter'] ? ' class="spaced"' : '') . '>' . htmlspecialchars ($committee['name']) . '</a>' . ($committee['staffOnly'] ? $restrictionMarkerHtml : '');
+				$list[] = ($committee['spaceAfter'] ? '<span class="spaced">' : '') . "<a href=\"{$committee['path']}/\"" . ($committee['isExternal'] ? ' target="_blank"' : '') . '>' . htmlspecialchars ($committee['name']) . '</a>' . ($committee['staffOnly'] ? $restrictionMarkerHtml : '') . ($committee['spaceAfter'] ? '</span>' : '');
 			}
-			$listingHtml .= application::htmlUl ($list);
+			$listingHtml .= application::htmlUl ($list, 2, 'index');
 		}
 		
 		# Compile the HTML

styles.css
@@ -1,4 +1,5 @@
-#committeelistings ul li a.spaced {margin-bottom: 1em; display: block;}
+#committeelistings ul.index li {margin-bottom: 0.2em;}
+#committeelistings ul.index li span.spaced {margin-bottom: 1.2em; display: block;}
 
 #committeelistings #cornersearch {position: relative; top: -12px; right: 0;}
 #committeelistings #editlink {clear: right; position: relative; top: -10px;}